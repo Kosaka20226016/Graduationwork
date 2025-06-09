45ce3556544bd7e5166c4363fc35986c0428993f
Kosaka20226016 <kosyou0108@gmail.com>
2025-06-09 11:49:43 +0900

    ログイン機能ほぼ完成

--- app/src/main/java/jp/ac/nkc_cta09/graduationwork/MainActivity.kt
+++ app/src/main/java/jp/ac/nkc_cta09/graduationwork/MainActivity.kt
@@ -1,11 +1,18 @@
 package jp.ac.nkc_cta09.graduationwork
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.common.BitMatrix
+import android.graphics.Bitmap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +26,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         //ここから書く
-        val database = FirebaseDatabase.getInstance()
-        val myRef =database.getReference("message")
+        val userId = FirebaseAuth.getInstance().currentUser?.uid ?: "UNKNOWN"
+        val barcodeImageView = findViewById<ImageView>(R.id.barcodeImageView)
 
-        myRef.setValue("こんにちはー")
+        val barcodeBitmap = generateBarcodeBitmap(userId)
+        barcodeImageView.setImageBitmap(barcodeBitmap)
+    }
 
-
+    fun generateBarcodeBitmap(content: String, width: Int = 1000, height: Int = 150): Bitmap {
+        val bitMatrix: BitMatrix = MultiFormatWriter().encode(
+            content,
+            BarcodeFormat.CODE_128,  // QRコードにしたいなら BarcodeFormat.QR_CODE に変更可能
+            width,
+            height
+        )
+        val encoder = BarcodeEncoder()
+        return encoder.createBitmap(bitMatrix)
     }
 }
