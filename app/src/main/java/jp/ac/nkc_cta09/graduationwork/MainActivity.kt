66bf2de0ea22b14cc0e90bc5b6f2b786e0b68079
Kosaka20226016 <kosyou0108@gmail.com>
2025-06-10 13:33:06 +0900

    0610メイン画面のデザインほぼ完成

--- app/src/main/java/jp/ac/nkc_cta09/graduationwork/MainActivity.kt
+++ app/src/main/java/jp/ac/nkc_cta09/graduationwork/MainActivity.kt
@@ -13,6 +13,8 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
 import android.graphics.Bitmap
+import android.widget.TextView
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +30,29 @@ class MainActivity : AppCompatActivity() {
         //ここから書く
         val userId = FirebaseAuth.getInstance().currentUser?.uid ?: "UNKNOWN"
         val barcodeImageView = findViewById<ImageView>(R.id.barcodeImageView)
-
+        val view_user_id = findViewById<TextView>(R.id.userid)
+        val view_user_name = findViewById<TextView>(R.id.username)
+        val database = FirebaseDatabase.getInstance().reference
         val barcodeBitmap = generateBarcodeBitmap(userId)
         barcodeImageView.setImageBitmap(barcodeBitmap)
+
+        database.child("users").child(userId).child("name").get()
+            .addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()) {
+                    val userName = dataSnapshot.getValue(String::class.java)
+                    view_user_name.text = "名前: $userName"
+                } else {
+                    view_user_name.text = "ユーザー名: 取得できませんでした"
+                }
+            }.addOnFailureListener {
+                view_user_name.text = "エラー: データ取得失敗"
+                Log.e("Firebase", "データ取得失敗", it)
+            }
+
+        view_user_id.text = "ID: $userId"
     }
 
-    fun generateBarcodeBitmap(content: String, width: Int = 1000, height: Int = 150): Bitmap {
+    fun generateBarcodeBitmap(content: String, width: Int = 1000, height: Int = 250): Bitmap {
         val bitMatrix: BitMatrix = MultiFormatWriter().encode(
             content,
             BarcodeFormat.CODE_128,  // QRコードにしたいなら BarcodeFormat.QR_CODE に変更可能
