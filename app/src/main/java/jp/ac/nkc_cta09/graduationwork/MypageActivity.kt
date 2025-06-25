56c2c5f576a2b970aee2a9c40ba8c08824380ddd
aiu20 <keigoinagaki5@gmail.com>
2025-06-25 12:37:45 +0900

    a

--- app/src/main/java/jp/ac/nkc_cta09/graduationwork/MypageActivity.kt
+++ app/src/main/java/jp/ac/nkc_cta09/graduationwork/MypageActivity.kt
@@ -12,6 +12,8 @@ import android.content.Intent
 class MypageActivity : AppCompatActivity() {
     private lateinit var btnChangeReservation: Button
     private lateinit var btnAccountSettings: Button
+    private lateinit var btnHome:Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +21,20 @@ class MypageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_mypage)
         btnChangeReservation = findViewById(R.id.btnChangeReservation)
         btnAccountSettings = findViewById(R.id.btnAccountSettings)
+        btnHome = findViewById(R.id.button4)
+
         btnChangeReservation.setOnClickListener {
             Toast.makeText(this, "予約変更・キャンセルボタンが押されました", Toast.LENGTH_SHORT).show()
         }
         btnAccountSettings.setOnClickListener {
             val intent = Intent(this, AccountSettingsActivity::class.java)
             startActivity(intent)
+        }
+        btnHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+            finish() // 現在のMyPageActivityを終了する
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
